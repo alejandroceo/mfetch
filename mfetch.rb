5e51e913982ae407644dabe91f71f028a63c1899
alejandroceo <101524105+alejandroceo@users.noreply.github.com>
2026-06-28 18:09:47 -0400

    Update mfetch.rb

--- mfetch.rb
+++ mfetch.rb
@@ -1,8 +1,8 @@
 class Mfetch < Formula
   desc "Informacion del sistema para macOS, estilo neofetch (logo + colores)"
-  homepage "https://github.com/TU_USUARIO/mfetch"
-  url "https://github.com/TU_USUARIO/mfetch/archive/refs/tags/v2.0.0.tar.gz"
-  sha256 "PEGA_AQUI_EL_SHA256_DEL_TARBALL"
+  homepage "https://github.com/alejandroceo/mfetch"
+  url "https://github.com/alejandroceo/mfetch/archive/refs/tags/v2.0.0.tar.gz"
+  sha256 "e51a4e346624a3bfeafad26e37f5a6a3496b051f9997f4a301ac4c697f900ea8"
   license "MIT"
 
   def install
